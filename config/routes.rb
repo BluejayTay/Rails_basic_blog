2c7125f287e0b0996c1b9d401e1f1055afca93de
BluejayTay <tayrusthoven@gmail.com>
2022-01-07 15:34:05 -0600

    Adds articles route,controller, view, model, db migration, adds first object to model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root "articles#index"
+
+  get "/articles", to: "articles#index"
+  #The route above declares that GET /articles requests are mapped to the index action of ArticlesController
+  get "/articles/:id", to: "articles#show"
+  # above is another GET route, but designates a route parameter, :id
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
